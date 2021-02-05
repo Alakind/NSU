bc1a57dd4337d7e973bd7a62dbbb86435c7f1b00
Alakind <sash.ivakin@yandex.ru>
2021-02-06 00:06:31 +0700

    set up folders

---
+++ C++/task0a/module1.cpp
@@ -0,0 +1,16 @@
+#include "module1.h"
+
+namespace Module1
+{
+	std::string getMyName()
+	{
+		std::string name = "John";
+		return name;
+	}
+
+	std::string getPeterMyName()
+	{
+		std::string name = "Peter";
+		return name;
+	}
+}
