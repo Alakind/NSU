de656191841bfd3f190a52acec2022b33c797447
Alakind <sash.ivakin@yandex.ru>
2021-03-13 19:32:57 +0700

    first version

--- opp/lab1/main1.c
+++ opp/lab1/main1.c
@@ -30,6 +30,12 @@ void matrix_copy(double** matrix1, double** matrix2, int n) {
     }
 }
 
+void vector_copy(double* vector1, double* vector2, int n) {
+    for (int i = 0; i < n; i++) {
+        vector1[i] = vector2[i];
+    }
+}
+
 void make_one_two_matrix(double** matrix, int n) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
@@ -75,6 +81,12 @@ void matrix_mul_double(double** matrix, double dub, int n) {
     }
 }
 
+void vector_mul_double(double* vector, double dub, int n) {
+    for (int i = 0; i < n; i++) {
+        matrix[i] *= dub;
+    }
+}
+
 void vector_minus_vector(double* vector1, double* vector2, int n) {
     for (int i = 0; i < n; i++) {
         vector1[i] -= vector2[i];
@@ -91,17 +103,47 @@ int is_finished(double** matrix, double* vector, double* b, double epsilon, int 
     matrix_mul_vector(A, vector, Ax, n);
     vector_minus_vector(Ax, b, n);
 
-    if ((scalar_mul(Ax, Ax, n) / scalar_mul(b, b, n)) < epsilon) {
+    if (sqrt(scalar_mul(Ax, Ax, n)) / sqrt(scalar_mul(b, b, n))) < epsilon) {
         return 1;
     }
 
     return 0;
 }
 
-/*double* solve_eq(double** matrix, double* values) {
+void count_y(double* y, double** A, double* x_n, double* b){
+    matrix_mul_vector(A, x, y, n);
+
+    vector_minus_vector(y, b, n);
+}
+
+void count_tau(double** A, double* y, int n) {
+    double* Ay = (double*)malloc(n * sizeof(double));
+    matrix_mul_vector(A, y, Ay, n);
+
+    double tau = (scalar_mul(y, Ay) / scalar_mul(Ay, Ay));
+
+    free(Ay);
+    return tau;
+}
+
+double* solve_eq(double** matrix, double* values, int n) {
     double epsilon = 0.001;
 
-}*/
+    double* y = (double*)malloc(n * sizeof(double));
+    double* x_i = (double*)malloc(n * sizeof(double));
+    vector_copy(x_i, values, n);
+
+    while (!is_finished(matrix, )) {
+        count_y(y, matrix, x_i, values);
+
+        double tau = count_tau(matrix, y, n);
+
+        vector_mul_double(y, tau);
+
+        vector_minus_vector(x_i, y);
+    }
+
+}
 
 int main() {
     // SETTING UP
