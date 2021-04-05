aa83172e6efac5753c37cbd5fca6edb6d2034cff
Alakind <sash.ivakin@yandex.ru>
2021-04-05 12:05:49 +0700

    made matrixies

--- opp/lab3/main.c
+++ opp/lab3/main.c
@@ -13,7 +13,31 @@ int main(int argc, char** argv) {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    
+    // Input
+    printf("Enter n1, n2, n3\n");
+    int n1;
+    int n2;
+    int n3;
+    scanf("%d", &n1);
+    scanf("%d", &n2);
+    scanf("%d", &n3);
 
+    double* A_matrix = (double*)malloc(n1 * n2);
+    double* B_matrix = (double*)malloc(n3 * n2);
+
+    printf("Enter p1\n");
+    int p1;
+    int p2;
+    scanf("%d", &p1);
+    p2 = proc_num / p1;
+
+    // Paralleling
+    double* hold_matrix = (double*)malloc(p1 * sizeof(double));
+    MPI_Scatter(A_matrix, p1, MPI_DOUBLE, hold_matrix, p1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+
+
+    free(A_matrix);
+    free(B_matrix);
     return 0;
 }
