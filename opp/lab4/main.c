2a8976c99c562e9c24ac2693c43d48e5e6c1a8ae
Alakind <sash.ivakin@yandex.ru>
2021-05-18 22:48:02 +0700

    lab4

--- opp/lab4/main.c
+++ opp/lab4/main.c
@@ -8,10 +8,11 @@
 MPI_Request send_request[2];
 MPI_Request recieve_request[2];
 
-int old_matrix = 0;
-int new_matrix = 1;
+int old = 0;
+int new = 1;
 
 double a = 100000;
+double epsilon = 0,00000001;
 
 double get_coordinates(int proc_amount, int coord) {
     return -1 + coord * (2 / (proc_amount));
@@ -50,29 +51,47 @@ int fill_matrix(float* matrix, int Nx, int Ny, int Nz, int rank, int proc_num) {
     }
 }
 
-int send_recv_jacobi(double* send_matrix, double* recv_matrix, int Nx, int Ny, int Nz, int rank, int proc_num) {
+void send_recv_jacobi(double* send_matrix, double* recv_matrix, int Nx, int Ny, int Nz, int rank, int proc_num) {
     if (rank != proc_num - 1) {
         MPI_Isend(send_matrix, Nx * Ny, MPI_DOUBLE, rank + 1, 1, MPI_COMM_WORLD, &send_request[1]);
         MPI_Irecv(recv_matrix[1], Nx * Ny, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD, &recieve_request[1]);
     }
     if (rank != 0) {
         MPI_Isend(send_matrix, Nx * Ny, MPI_DOUBLE, rank - 1, 1, MPI_COMM_WORLD, &send_request[0]);
-        MPI_Irecv(recv_matrix[0], Nx * Ny, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, &recieve_request[1]);
+        MPI_Irecv(recv_matrix[0], Nx * Ny, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, &recieve_request[0]);
     }
 }
 
-int count_center(double* matrix, int Nx, int Ny, int Nz) {
+void count_center(double* matrix, int Nx, int Ny, int Nz, int* finish) {
     double hx = 2 / (Nx - 1);
     double hy = 2 / (Ny - 1);
     double hz = 2 / (Nz - 1);
 
-    for (int i = 0; i < Nx; i++) {
-        for (int j = 0; j < Ny; j++) {
-            for (int k = 0; k < Nz / proc_num; k++) {
-                matrix[i * Nx * Ny + j * Ny + k] = ((matrix) / (hx * hx) + ) / 
-                                                    (2 / (hx * hx) + 2 / (hy * hy) + 2 / (hz * hz) + a);
+    for (int i = 1; i < Nx - 1; i++) {
+        for (int j = 1; j < Ny - 1; j++) {
+            for (int k = 1; k < Nz / proc_num - 1; k++) {
+                matrix[1][i * Nx * Ny + j * Ny + k] = ((matrix[0][(i - 1) * Nx * Ny + j * Ny + k] + matrix[0][(i + 1) * Nx * Ny + j * Ny + k]) / (hx * hx) 
+                                                     + (matrix[0][i * Nx * Ny + (j + 1) * Ny + k] + matrix[0][i * Nx * Ny + (j - 1) * Ny + k]) / (hy * hy)
+                                                     + (matrix[0][i * Nx * Ny + j * Ny + k + 1] + matrix[0][i * Nx * Ny + j * Ny + k - 1]) / (hz * hz)) / 
+                                                    (2 / (hx * hx) + 2 / (hy * hy) +
+                                                     2 / (hz * hz) + a);
             }
         }
+    }
+
+    if (abs(matrix[1][i * Nx * Ny + j * Ny + k] - matrix[0][i * Nx * Ny + j * Ny + k]) > epsilon) {
+        *finish = 0;
+    }
+}
+
+void wait_data(int rank, int proc_num) {
+    if (rank != proc_num - 1) {
+        MOI_Wait(&send_request[1]);
+        MPI_Wait(&recieve_request[1]);
+    }
+    if (rank != 0) {
+        MPI_Wait(&send_request[0]);
+        MPI_Wait(&recieve_request[0]);
     }
 }
 
@@ -96,7 +115,7 @@ int main(int argc, char** argv) {
 
         // COUNT CENTER (count_center)
 
-        // WAIT
+        // WAIT (wait_data)
 
         // COUNT EDGE
     //}
