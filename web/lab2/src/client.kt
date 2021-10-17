3167d7feb0091a6ffdff10776bb169108aa0ca54
Alexander Ivakin <sash.ivakin@yandex.ru>
2021-10-17 22:22:36 +0700

    server + client

--- web/lab2/src/client.kt
+++ web/lab2/src/client.kt
@@ -2,21 +2,42 @@ package src
 
 import java.io.OutputStream
 import java.net.*
+import java.io.*
 import java.util.*
+
+val BUFF = 100;
 
 // USE
 // 127.0.0.1
 // 8800
 fun main(args: Array<String>) {
-    if (args.size < 2) {
-        println("Enter adress and port as arguments");
+    if (args.size < 3) {
+        println("Enter adress, port and path to file as arguments");
         return;
     }
 
-    val adress = args[0];
-    val port = args[1];
+    val adress: String = args[0];
+    val port: String = args[1];
+    val filename: String = args[2];
+
+    val charset = Charsets.UTF_8;
 
     val ip: InetAddress = InetAddress.getByName(adress);
+    val client = Socket(ip, port.toInt());
+    val output: OutputStream = client.getOutputStream();
 
-    val client = Socket(adress, port);
+    // sending name of file
+    output.write((filename + "\n").toByteArray(charset));
+
+    // sending size pf file
+    output.write((File(filename).length().toString() + "\n").toByteArray(charset));
+
+    // reading and sending file
+    val inputStream: InputStream = File(filename).inputStream();
+
+    val buffer = ByteArray(BUFF);
+    while(inputStream.read(buffer, 0, BUFF) != -1) {
+        output.write(buffer);
+    }
+    output.write(buffer);
 }
--- web/lab2/src/server.kt
+++ web/lab2/src/server.kt
@@ -3,7 +3,9 @@ package src
 import java.io.OutputStream
 import java.net.*
 import java.util.*
+import java.io.*
 
+val BUFF = 100;
 const val backlog = 10;
 
 // USE
@@ -25,6 +27,24 @@ fun main(args: Array<String>) {
     val client = server.accept();
     println("Client connected");
 
+    // reading name and size
+    val input = BufferedReader(InputStreamReader(client.getInputStream()));
+
+    val filename = input.readLine();
+    println(filename);
+    println(input.readLine());
+
+    // making file
+    val fileStream: OutputStream = File(filename + "hehe").outputStream();
+
+    // reading file
+    val inputStream = client.getInputStream();
+
+    val buffer = ByteArray(BUFF);
+    while(inputStream.read(buffer, 0, BUFF) != -1) {
+        fileStream.write(buffer);
+    }
+    fileStream.write(buffer);
 
     println("I'm done");
 }
