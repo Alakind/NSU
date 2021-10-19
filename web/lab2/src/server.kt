8fd42fdff0d3ca94b3400bb512af8b21fb9a8b2c
Alexander Ivakin <sash.ivakin@yandex.ru>
2021-10-19 17:08:17 +0700

    server

--- web/lab2/src/server.kt
+++ web/lab2/src/server.kt
@@ -4,10 +4,84 @@ import java.io.OutputStream
 import java.net.*
 import java.util.*
 import java.io.*
+import java.lang.Thread
 
 val BUFF = 4096;
 
 const val backlog = 10;
+val charset = Charsets.UTF_8;
+
+class SpeedInfo(download: Long, fSize: Long) {
+    public var downloaded: Long = download;
+    public var fullSize: Long = fSize;
+    private var secondsSpent: Long = 0;
+
+    public fun averageSpeed(): Long {
+        return downloaded / secondsSpent;
+    }
+    public fun addSeconds(seconds: Long) {
+        secondsSpent += seconds;
+    }
+}
+
+fun printInfoThread(speedInfo: SpeedInfo) {
+    Thread.sleep(3000);
+    speedInfo.addSeconds(3);
+
+    println(speedInfo.downloaded.toString() + " / " + speedInfo.fullSize.toString() + " bytes");
+    println("Average speed: " + speedInfo.averageSpeed().toString());
+}
+
+fun clientRun(server: ServerSocket){
+
+    val client = server.accept();
+    println("Client connected");
+
+    val buffer = ByteArray(BUFF);
+
+    // reading name and size
+    val input = client.getInputStream();
+
+    // reading size
+    input.read(buffer, 0, BUFF);
+    val size: Long = (buffer.toString(charset).split("\n"))[0].toLong();
+
+    // readin name
+    val curDir: String = (System.getProperty("user.dir"));
+
+    input.read(buffer, 0, BUFF);
+    val filenameCome: String = (buffer.toString(charset).split("\n"))[0];
+    val filename: String = curDir + "/downloads/" + (filenameCome.split("/")).last();
+
+    // Showing speed info
+    val speedInfo = SpeedInfo(0, size);
+    val thread: Thread = Thread(Runnable {
+        printInfoThread(speedInfo);
+    });
+    thread.start();
+
+    // making file
+    val fileStream: OutputStream = File(filename).outputStream();
+
+    // reading file
+    val inputStream = client.getInputStream();
+
+
+
+    var left: Long = size;
+    while(left > 0) {
+        var readed: Int = inputStream.read(buffer, 0, BUFF);
+        left -= readed;
+
+        speedInfo.downloaded = size - left;
+
+        fileStream.write(buffer, 0, readed);
+    }
+
+    thread.join();
+
+    println("File sent successfully!");
+}
 
 // USE
 // 127.0.0.1
@@ -20,45 +94,20 @@ fun main(args: Array<String>) {
 
     val adress = args[0];
     val port = args[1];
-    val buffer = ByteArray(BUFF);
-    val charset = Charsets.UTF_8;
 
     val ip: InetAddress = InetAddress.getByName(adress);
 
     val server = ServerSocket(port.toInt(), backlog, ip);
 
-    val client = server.accept();
-    println("Client connected");
+    var threads: Vector<Thread> = Vector<Thread>();
 
-    // reading name and size
-    val input = client.getInputStream();
+    while (true) {
+        for (i in 1..backlog) {
+            threads.add(Thread(Runnable {
+                clientRun(server);
+            }));
 
-    // reading size
-    input.read(buffer, 0, BUFF);
-    val size: Long = (buffer.toString(charset).split("\n"))[0].toLong();
-    println(size);
-
-    // readin name
-    val curDir: String = (System.getProperty("user.dir"));
-
-    input.read(buffer, 0, BUFF);
-    val filenameCome: String = (buffer.toString(charset).split("\n"))[0];
-    val filename: String = curDir + "/downloads/" + (filenameCome.split("/")).last();
-    println(filename);
-
-    // making file
-    val fileStream: OutputStream = File(filename).outputStream();
-
-    // reading file
-    val inputStream = client.getInputStream();
-
-    var left: Long = size;
-    while(left > 0) {
-        var readed: Int = inputStream.read(buffer, 0, BUFF);
-        left -= readed;
-
-        fileStream.write(buffer, 0, readed);
+            threads.lastElement().start();
+        }
     }
-
-    println("I'm done");
 }
