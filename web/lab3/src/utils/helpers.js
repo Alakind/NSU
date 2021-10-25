e302fbeb255cf5284609ab0c9da004ecd73d9f91
Alakind <sash.ivakin@yandex.ru>
2021-10-25 23:20:57 +0700

    error handling

--- web/lab3/src/utils/helpers.js
+++ web/lab3/src/utils/helpers.js
@@ -3,14 +3,23 @@ import { openTripApi, openWeatherApi } from ".";
 
 const getFullInfo = async function(city, setWeather, setPlaces) {
     const promiseWeather = openWeatherApi.fetchOpenWeatherByName(city, consts.API_KEY_OPEN_WEATHER);
-    
+
+    let weather;
+    let places;
+
+    try {
     const responseW = await promiseWeather;
-    const weather = await responseW.json();
+    weather = await responseW.json();
 
     const promisePlaces = openTripApi.fetchOpenTripRadius(weather.coord.lon, weather.coord.lat, consts.RADIUS);
 
     const responseP = await promisePlaces;
-    const places = await responseP.json();
+    places = await responseP.json();
+    } catch(error) {
+        console.log(error);
+
+        return;
+    }
 
     setWeather(weather);
     setPlaces(places);
@@ -18,9 +27,17 @@ const getFullInfo = async function(city, setWeather, setPlaces) {
 
 const fetchPlace = async function(id, setPlace) {
     const promise = openTripApi.fetchOpenTripPlace(id);
+    
+    let place;
 
-    const response = await promise;
-    const place = await response.json();
+    try {
+        const response = await promise;
+        place = await response.json();
+    } catch(error) {
+        console.log(error);
+
+        return;
+    }
 
     setPlace(place);
 }
