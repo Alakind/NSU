e9840781fb37cb36eb191bd7a629ad5938c8599e
Alexander Ivakin <sash.ivakin@yandex.ru>
2021-12-07 16:39:02 +0700

    fix

--- web/lab4/src/main/kotlin/view/Playfield.kt
+++ web/lab4/src/main/kotlin/view/Playfield.kt
@@ -2,18 +2,11 @@ package ru.ivakin.snake.view
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.*
-// import androidx.compose.material.Button
-// import androidx.compose.material.Slider
-// import androidx.compose.material.Text
 import androidx.compose.foundation.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-// import androidx.compose.ui.layout.onSizeChanged
-// import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.*
-// import kotlin.random.Random
-// import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.*
 import androidx.compose.foundation.shape.*
 import androidx.compose.ui.*
