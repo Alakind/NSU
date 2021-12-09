d319d0c98e92ce8b87b84fade01dd563276f1106
Alexander Ivakin <sash.ivakin@yandex.ru>
2021-12-09 12:22:51 +0700

    wip: snake class

--- web/lab4/src/main/kotlin/ru/ivakin/snake/model/Snake.kt
+++ web/lab4/src/main/kotlin/ru/ivakin/snake/model/Snake.kt
@@ -1,10 +1,14 @@
-package ru.ivakin.snake.model
+package ru.ivakin.snake.model;
+
+import java.util.Vector;
 
 import ru.ivakin.snake.proto.SnakesProto;
 import ru.ivakin.snake.proto.SnakesProto.GameState.Snake.SnakeState;
 import ru.ivakin.snake.proto.SnakesProto.Direction;
 
-class SnakeModel(val playerId: Int) {
+class SnakeModel(val playerId: Int, val head: Point) {
     var state: SnakeState = SnakeState.ALIVE;
     var direction: Direction = Direction.UP;
+
+    var points: Vector<Point> = Vector<Point>();
 }
