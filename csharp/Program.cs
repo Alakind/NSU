b8960a044c456e97b70988e671f31c733af54875
Alexander Ivakin <sash.ivakin@yandex.ru>
2022-09-16 13:22:12 +0700

    added C# folder

---
+++ csharp/Program.cs
@@ -0,0 +1,5 @@
+﻿using view;
+
+ConsoleView consoleView = new ConsoleView();
+
+consoleView.greet();
---
+++ csharp/src/view/ConsoleView.cs
@@ -0,0 +1,17 @@
+namespace view;
+class ConsoleView
+{
+    public void greet()
+    {
+        Console.WriteLine(@"Welcome Picky Princess's Castle!
+        ┈┈┈┈▅┈┈▕▀┈┈┈┈┈┈
+        ┈┈┈▕┈┈┈╱╲▕▀┈┈┈┈
+        ┈┈┈╱╲┈┈▏▕╱╲┈┈┈┈
+        ┈┈┈▏▕╱╲▏▎▏▕╱╲┈▃
+        ┈╱╲▏▎▅▂▅▂▏▎▏▎▏▏
+        ▂▏▎▏▕╭┳┳╮▏┊▏▕╱╲
+        ▏▏┊▏▎┃┊┊┃▏▎▏▎▏▕
+        ▇▆▅▃▂┻┻┻┻▂▃▅▆▇▉
+");
+    }
+}
