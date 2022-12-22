b42d88eaab6a7924e9963aaeb2751109793e3c55
Alexander Ivakin <sash.ivakin@yandex.ru>
2022-12-22 20:48:31 +0700

    added fluent assertions

--- csharp/PickyPrincessTest/CharacterGeneratorTest.cs
+++ csharp/PickyPrincessTest/CharacterGeneratorTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using util;
 using model;
 using mocks;
+using FluentAssertions;
 
 namespace PickyPrincessTest;
 
@@ -15,16 +16,6 @@ public class CharacterGeneratorTest
         var generator = new CharacterGeneratorMock();
 
         Character[] characters = generator.GetCharactersList();
-        var isCharacterBeen = new Dictionary<string, bool>();
-        for (int i = 0; i < characters.Length; i++) {
-            isCharacterBeen.Add(characters[i].Name, false);
-        }
-
-        for (int i = 0; i < characters.Length; i++) {
-            Assert.AreEqual(isCharacterBeen[characters[i].Name], false);
-
-            isCharacterBeen.Remove(characters[i].Name);
-            isCharacterBeen.Add(characters[i].Name, true);
-        }
+        characters.Should().OnlyHaveUniqueItems();
     }
 }
